e8d9391ec5ff73976f4f659a6abb86dbf4355cfb
Jessica Gilligan <jessicagilligan@lawn-143-215-50-24.lawn.gatech.edu>
2017-04-18 19:24:15 -0400

    2 servers bish

---
+++ twoServers.js
@@ -0,0 +1,29 @@
+//need a protocol
+var http = require('http');
+
+//need a door to the house
+var PORT1 = 7000;
+var PORT2 = 7050;
+
+//here's the callback
+function serverResponse1( request, response ) {
+    response.end('You are the best human being in the world!  You hit the good port:' + request.url);
+}
+function serverResponse2( request, response ) {
+    response.end('You are the worst person ever!  You hit the shitty port:' + request.url);
+}
+
+//build the server which accepts callback (see aboce)
+var server1 = http.createServer(serverResponse1); 
+var server2 = http.createServer(serverResponse2);
+
+//need to be able to listen in a server
+server1.listen(PORT1, function () {
+//when we start listening... do something when we start up the server
+    console.log('server has been started, listening on: http://localhost:' + PORT1);
+})
+
+server2.listen(PORT2, function () {
+//when we start listening... do something when we start up the server
+    console.log('server has been started, listening on: http://localhost:' + PORT2);
+})
